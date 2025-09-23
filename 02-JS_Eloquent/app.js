28d9d1bbcb0c7ccb1bbb2775e208f3a08587fd8e
joshdels <deleonjoshy2@gmail.com>
2025-09-23 23:36:57 +0800

    start of js eloquent for better js

---
+++ 02-JS_Eloquent/app.js
@@ -0,0 +1,13 @@
+let num = 0;
+while (num <= 12) {
+  console.log(num);
+  num = num + 2;
+}
+
+// Forloops
+for (let i = 9; ;i = i + 1) {
+  if (i % 8 == 0) {
+    console.log(i);
+    break;
+  }
+}
